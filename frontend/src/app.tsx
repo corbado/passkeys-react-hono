3fb515b6d763626992c283e0133b4f576f4b49dd
Lukas Kratzel <lukas.kratzel2004@gmail.com>
2025-01-08 08:48:32 +0100

    Automated commit

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -19,8 +19,6 @@ export default function App() {
             projectId={import.meta.env.VITE_CORBADO_PROJECT_ID}
             // enable dark mode for the Corbado UI
             darkMode="on"
-            // store the users session-token in a cookie called 'cbo_session_token'
-            setShortSessionCookie
             // apply our custom styles to the Corbado UI
             theme="cbo-custom-styles"
             // use our custom translations
