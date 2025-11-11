98c65f8d9c2ec65369960459cd97bb87f7681c25
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-11-11 10:10:52 +0100

    Automated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,6 +32,8 @@ class App {
             cors({
                 origin: "http://localhost:3000",
                 credentials: true,
+                allowHeaders: ["Content-Type", "Authorization"],
+                allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             }),
         );
         this.app.use(authenticationMiddleware);
